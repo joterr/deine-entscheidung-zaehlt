7796d045adfa07c3de8552da1d5b35a7793b834d
Jannik Rulitschka <jannik.rulitschka@mhp.de>
2018-10-19 15:21:08 +0200

    added truth overlay

---
+++ src/components/TruthOverlay.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="truth-overlay" v-bind:class="{ opened:  isVisible }">
+    TEST<br/>
+    TEST<br/>
+    TEST<br/>
+    TEST<br/>
+    TEST<br/>
+    TEST<br/>
+    TEST
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class TruthOverlay extends Vue {
+  @Prop()
+  isVisible: boolean = false;
+}
+</script>
+
+<style scoped lang="scss">
+  .truth-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 9999;
+    background-color: $cl-accent1;
+    opacity: 0.98;
+    padding: 8vh 8vw;
+    display: none;
+
+    &.opened {
+      display: block;
+    }
+  }
+</style>
--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -2,21 +2,31 @@
   <section class="intro-page intro page">
     <div class="fact-wrapper">
       <FactSentence />
-      <p class="description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. <a class="show-truth">Findest Du nicht okay?</a></p>
+      <p class="description">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. <a class="show-truth" v-on:click="openTruth">Findest Du nicht okay?</a></p>
     </div>
+    <TruthOverlay v-bind:isVisible="truthOpened"/>
   </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Provide } from "vue-property-decorator";
 import FactSentence from "@/components/FactSentence.vue";
+import TruthOverlay from "@/components/TruthOverlay.vue";
 
 @Component({
   components: {
-    FactSentence
+    FactSentence,
+    TruthOverlay
   }
 })
-export default class Intro extends Vue {}
+export default class Intro extends Vue {
+  @Provide()
+  private truthOpened = false;
+
+  public openTruth() {
+    this.truthOpened = true;
+  }
+}
 </script>
 
 <style scoped lang="scss">
