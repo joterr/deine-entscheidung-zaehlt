3fdf576877d0d8cc15788dea14f8cc54dbeb2ff7
Jannik Rulitschka <jannik.rulitschka@mhp.de>
2019-08-18 15:09:06 +0200

    Add type for vue-countup-v2

---
+++ src/@types/vue-countup-v2/index.d.ts
@@ -0,0 +1,8 @@
+declare module "vue-countup-v2" {
+  export interface CountUpV2Options {
+    useEasing?: boolean;
+    useGrouping?: boolean;
+    separator?: string;
+    decimal?: string;
+  }
+}
