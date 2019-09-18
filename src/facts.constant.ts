e900c93d214ab6ad304a2173a6d2c3d7d97cf570
Jannik Rulitschka <jannik.rulitschka@mhp.de>
2019-09-18 23:48:57 +0200

    Outsource facts / sources to constant file

---
+++ src/facts.constant.ts
@@ -0,0 +1,248 @@
+// tslint:disable:max-line-length
+
+export const ENVIRONMENTAL_IMPACTS = [
+  {
+    desc: "Fleisch von kranken Tieren",
+    pub: "FAZ",
+    date: "22. Sep 2016",
+    link: "http://www.faz.net/aktuell/wirtschaft/wirtschaftspolitik/kritik-an-tierhaltung-fleisch-von-kranken-tieren-14448092.html"
+  },
+  {
+    desc: "Klimawandel: Fleischkonzerne schaden dem Klima mehr als die Ölindustrie",
+    pub: "Süddeutsche",
+    date: "18. Jul 2018",
+    link: "https://www.sueddeutsche.de/wirtschaft/klimawandel-fleischkonzerne-co-emissionen-1.4058225"
+  },
+  {
+    desc: "Nitrat im Grundwasser durch Überdüngung und Gülle",
+    pub: "Spiegel Online",
+    date: "01. Mai 2015",
+    link: "http://www.spiegel.de/wirtschaft/service/nitrat-im-grundwasser-durch-ueberduengung-und-guelle-a-1027279.html"
+  },
+  {
+    desc: "Wasser: Das größte Problem heißt Massentierhaltung",
+    pub: "Frankfurter Rundschau",
+    date: "20. Mär 2018",
+    link: "http://www.fr.de/wirtschaft/wasser-das-groesste-problem-heisst-massentierhaltung-a-1470284"
+  },
+  {
+    desc: "EuGH verurteilt Deutschland wegen zu viel Nitrat im Grundwasser",
+    pub: "Süddeutsche",
+    date: "21. Jun 2018",
+    link: "https://www.sueddeutsche.de/wirtschaft/europaeischer-gerichtshof-verurteilt-deutschland-nitrat-grundwasser-1.4025538"
+  },
+  {
+    desc: "Fleischfabrik Deutschland - Welche Folgen hat die Massentierhaltung?",
+    pub: "Tagesspiegel",
+    date: "06. Jun 2016",
+    link: "https://www.tagesspiegel.de/politik/fleischfabrik-deutschland-welche-folgen-hat-die-massentierhaltung/13690370.html"
+  },
+  {
+    desc: "Antibiotika: Massentierhaltung fördert resistente Keime weltweit",
+    pub: "Zeit Online",
+    date: "01. Mai 2015",
+    link: "https://www.zeit.de/wissen/2014-05/antibiotika-resistenzen-resistente-keime-who"
+  },
+  {
+    desc: "Industrielle Tierhaltung braucht Antibiotika – und erhöht das Risiko resistenter Bakterien [Putenfleisch von Aldi, Lidl & Co. mit antibiotika­resistenten Keimen belastet]",
+    pub: "BUND",
+    date: "",
+    link: "https://www.bund.net/massentierhaltung/antibiotika/"
+  },
+  {
+    desc: "Wie sich die industrielle Tierhaltung auf den Klimawandel auswirkt",
+    pub: "",
+    date: "proveg international",
+    link: "https://vebu.de/tiere-umwelt/umweltbelastung-durch-fleischkonsum/klimawandel-industrielle-landwirtschaft-als-ursache/"
+  },
+  {
+    desc: "Umweltbelastung: Folgen des Fleischkonsums",
+    pub: "proveg international",
+    date: "",
+    link: "https://vebu.de/tiere-umwelt/umweltbelastung-durch-fleischkonsum/"
+  },
+  {
+    desc: "Ökologische Aspekte der Massentierhaltung auf globaler und überregionaler Ebbene",
+    pub: "WWF Deutschland",
+    date: "27. Aug 2013",
+    link: "https://www.bfn.de/fileadmin/MDB/documents/ina/vortraege/2013/2013-nachhaltige-LebensstileIII_Wolter.pdf"
+  }
+];
+
+export const VIOLATIONS_HANDLING_SCANDALS = [
+  {
+    desc: "Fleisch von kranken Tieren",
+    pub: "FAZ",
+    date: "22. Sep 2016",
+    link: "http://www.faz.net/aktuell/wirtschaft/wirtschaftspolitik/kritik-an-tierhaltung-fleisch-von-kranken-tieren-14448092.html"
+  },
+  {
+    desc: "Rinder in Bio-Schlachthof offenbar nicht fachgerecht betäubt",
+    pub: "RBB 24",
+    date: "08. Nov 2018",
+    link: "https://www.rbb24.de/panorama/beitrag/2018/11/oeko-schlachter-tierquaelerei-brandenburg.html"
+  },
+  {
+    desc: "Tierquälerei in der Schweinemast – garniert mit Gütesiegel",
+    pub: "Stern",
+    date: "19. Okt 2016",
+    link: "https://www.stern.de/tv/schweinemast-in-merklingen---so-grauenvoll-werden-die-tiere-gehalten-7106094.html"
+  },
+  {
+    desc: "Bauern dürfen Ferkel weiter ohne Betäubung kastrieren",
+    pub: "FAZ",
+    date: "04. Nov 2018",
+    link: "http://www.faz.net/aktuell/wirtschaft/koalition-erlaubt-ferkelkastration-ohne-betaeubung-weiter-15873423.html"
+  },
+  {
+    desc: "Der Schmerz zählt nicht",
+    pub: "Zeit Online",
+    date: "07. Nov 2018",
+    link: "https://www.zeit.de/2018/46/ferkelkastration-betaeubung-tierrechte-union-spd-bundestag"
+  },
+  {
+    desc: "VERGAST ODER LEBEND ENTSORGT: HEIMLICH-VIDEO ZEIGT KÜKEN-STERBEN",
+    pub: "Bild",
+    date: "09. Apri 2018",
+    link: "https://www.bild.de/regional/muenchen/tierschutz/kueken-werden-getoetet-55327016.bild.html"
+  },
+  {
+    desc: "Kükenshreddern - Das Gemetzel geht weiter",
+    pub: "Süddeutsche",
+    date: "29. Mär 2018",
+    link: "https://www.sueddeutsche.de/wirtschaft/kuekenschreddern-das-gemetzel-geht-weiter-1.3924618"
+  },
+  {
+    desc: "Kollateralschäden der Fleischindustrie: Millionen Schweine jährlich sterben qualvoll vor der Schlachtung",
+    pub: "ARIWA",
+    date: "20. Mär 2018",
+    link: "http://www.ariwa.org/aktivitaeten/aufgedeckt/recherchearchiv/1577-2018-03-19-17-06-57.html"
+  },
+  {
+    desc: "Untersuchungen an verendeten/getöteten Schweinen in Verarbeitungsbetrieben für tierische Nebenprodukte",
+    pub: "Tierärztliche Hochschule Hannover",
+    date: "12. Dez 2017",
+    link: "https://www.tiho-hannover.de/aktuelles-presse/aktuelle-meldungen/aktuelle-meldungen/article/untersuchungen-an-verendeteng-1/"
+  },
+  {
+    desc: "Video deckt auf: Tierquälerei in einem der größten deutschen Milchbetriebe",
+    pub: "Ökotest",
+    date: "09. Jul 2019",
+    link: "https://www.oekotest.de/essen-trinken/Video-deckt-auf-Tierquaelerei-in-einem-der-groessten-deutschen-Milchbetriebe-_10741_1.html"
+  },
+  {
+    desc: "Dreckiges Geschäft zum Thema Milch",
+    pub: "MDR",
+    date: "09. Okt 2018",
+    link: "https://www.ardmediathek.de/tv/FAKT/Dreckiges-Gesch%C3%A4ft/Das-Erste/Video?bcastId=310854&documentId=56797322"
+  },
+  {
+    desc: "Leberentzündung durch Fleisch: Jede fünfte Wurst ist mit Hepatitis-E-Viren belastet",
+    pub: "Focus Online",
+    date: "10. Mai 2016",
+    link: "https://www.focus.de/gesundheit/ernaehrung/gesundessen/salami-leberwurst-mettbroetchen-leberentzuendung-durch-fleisch-jede-fuenfte-wurst-ist-mit-hepatitis-e-viren-belastet_id_5519027.html"
+  },
+  {
+    desc: "Millionen Schweine sterben für den Müll",
+    pub: "ARD",
+    date: "20. Mär 2018",
+    link: "https://www.ardmediathek.de/tv/REPORT-MAINZ/Millionen-Schweine-sterben-f%C3%BCr-den-M%C3%BCll/Das-Erste/Video?bcastId=310120&documentId=50999848"
+  },
+  {
+    desc: "Skandal um das Ferkeltöten [Ferkel werden auf den Boden geschlagen und in den Eimer geworfen...]",
+    pub: "MDR",
+    date: "02. Aug 2017",
+    link: "https://www.mdr.de/investigativ/illegal-ferkel-tot-tierrecht-100.html"
+  },
+  {
+    desc: "Hunderte Schweine im Stall erstickt",
+    pub: "MDR",
+    date: "12. Sep 2018",
+    link: "https://www.mdr.de/thueringen/sued-thueringen/hildburghausen/schweine-in-stall-erstickt-kaesslitz-100.html"
+  },
+  {
+    desc: "KRITIK AN TIERHALTUNG: Fleisch von kranken Tieren",
+    pub: "FAZ",
+    date: "22. Sep 2016",
+    link: "http://www.faz.net/aktuell/wirtschaft/wirtschaftspolitik/kritik-an-tierhaltung-fleisch-von-kranken-tieren-14448092.html"
+  },
+  {
+    desc: "Pillen für das Turbo-Schwein",
+    pub: "Spiegel",
+    date: "22. Jan 2001",
+    link: "http://www.spiegel.de/spiegel/print/d-18309047.html"
+  },
+  {
+    desc: "Massives Versagen der Überwachungsbehörde machte Tierquälerei möglich SOKO Tierschutz erstattet Strafanzeige gegen das Landratsamt Alb-Donau Kreis",
+    pub: "SOKO Tierschutz e.V.",
+    date: "21. Okt 2016",
+    link: "https://www.presseportal.de/pm/110736/3461938"
+  },
+  {
+    desc: "Verdeckte Videoaufnahmen: Hier werden kleine Ferkel einfach erschlagen",
+    pub: "utopia",
+    date: "19. Jul 2018",
+    link: "https://utopia.de/schweine-fleisch-ferkel-animal-rights-watch-video-97815/"
+  },
+  {
+    desc: "Fleisch kostet Leben",
+    pub: "PETA",
+    date: "Feb 2018",
+    link: "https://www.peta.de/Fleisch-kostet-Leben"
+  }
+];
+
+
+export const HEALTH_BENEFITS = [
+  {
+    desc: "Fleisch von kranken Tieren",
+    pub: "FAZ",
+    date: "22. Sep 2016",
+    link: "http://www.faz.net/aktuell/wirtschaft/wirtschaftspolitik/kritik-an-tierhaltung-fleisch-von-kranken-tieren-14448092.html"
+  },
+  {
+    desc: "Warum es das Beste wäre, wenn wir uns vegan ernähren würden",
+    pub: "Stern",
+    date: "01. Nov 2018",
+    link: "https://www.stern.de/genuss/essen/vegane-ernaehrung--darum-ist-sie-perfekt-fuer-unsere-zeit-6710708.html"
+  },
+  {
+    desc: "Gesundheitsgefahr durch Fleisch: Steaksteuer könnte Tausende retten",
+    pub: "TAZ",
+    date: "08. Nov 2018",
+    link: "https://taz.de/Gesundheitsgefahr-durch-Fleisch/!5546513/"
+  }
+];
+
+export const SLAUGHTER_FIGURES  = [
+  {
+    desc: "Fleisch von kranken Tieren",
+    pub: "FAZ",
+    date: "22. Sep 2016",
+    link: "http://www.faz.net/aktuell/wirtschaft/wirtschaftspolitik/kritik-an-tierhaltung-fleisch-von-kranken-tieren-14448092.html"
+  },
+  {
+    desc: "Pressemitteilung Nr. 38: Fleischerzeugung im Jahr 2017 deutlich gesunken",
+    pub: "DESTATIS",
+    date: "07. Feb 2018",
+    link: "https://www.destatis.de/DE/PresseService/Presse/Pressemitteilungen/2018/02/PD18_038_413.html"
+  },
+  {
+    desc: "Geflügelhof Frandsander, Unsere Tiere - Enten",
+    pub: "Geflügelhof Franzsander",
+    date: "10. Nov 2018",
+    link: "http://www.gefluegelhof-franzsander.de/enten.php"
+  },
+  {
+    desc: "Schlachtzahlen 2017: 8 Mio. Tiere weniger",
+    pub: "Albert Schweitzer Stiftung",
+    date: "10. Feb 2018",
+    link: "https://albert-schweitzer-stiftung.de/aktuell/schlachtzahlen-2017"
+  },
+  {
+    desc: "Schlachthaus-Interview mit einer Mitarbeiterin",
+    pub: "Albert Schweitzer Stiftung",
+    date: "24. Mär 2015",
+    link: "https://albert-schweitzer-stiftung.de/aktuell/schlachthaus-interview"
+  }
+];
